a907df9ce4d5e48bde212eaa13032992c8879f8d
joseph-468 <75801386+joseph-468@users.noreply.github.com>
2022-11-01 10:22:15 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from pytesseract import pytesseract
 import pyautogui
 from PIL import Image
 import time
-import random
 import threading
 import cv2
 import json
@@ -13,13 +12,24 @@ import json
 # Config
 with open("config.json") as file:
     config = json.load(file)
-coords = config["screenshotSize"][0]
+
+if config["RESET_CONFIG"] is True:
+    with open("config_default.json") as file:
+        default_settings = json.load(file)
+        with open("config.json", "w") as file:
+            json.dump(default_settings, file, indent=2)
+
+with open("config.json") as file:
+    config = json.load(file)
+
+coords = config["screenshotSize"]
 left, top, bottom, right = coords["left"], coords["top"], coords["bottom"], coords["right"]
 pytesseract.tesseract_cmd = config["tesseractPath"]
 path = config["screenshotPath"]
 correct_chars = config["correctChars"]
-typing_settings = config["typingSettings"][0]
+typing_settings = config["typingSettings"]
 typing_speed = (typing_settings["WPM"] - 1) * 1000
+file.close()
 
 # Setup
 mouse = MouseController()
@@ -46,14 +56,22 @@ def on_press(key):
     elif key == Key.page_up:
         print(f"Top left: (X: {mouse.position[0]}, Y: {mouse.position[1]})")
         left, top = mouse.position
+        config["screenshotSize"]["left"], config["screenshotSize"]["top"] = mouse.position
+        with open("config.json", "w") as file:
+            json.dump(config, file, indent=2)
+        file.close()
+
     elif key == Key.page_down:
         print(f"Bottom right: (X: {mouse.position[0]}, Y: {mouse.position[1]})")
         right, bottom = mouse.position
+        config["screenshotSize"]["right"], config["screenshotSize"]["bottom"] = mouse.position
+        with open("config.json", "w") as file:
+            json.dump(config, file, indent=2)
+        file.close()
 
 
-def type_sentence():
-    global running
-    # Get text from screen
+def get_text_from_image():
+    # Take screenshot
     pyautogui.screenshot(path)
     im = Image.open(path)
     im = im.crop((left, top, right, bottom))
@@ -61,9 +79,15 @@ def type_sentence():
     img = cv2.imread("screenshot.png")
     # Clean text
     sentence = pytesseract.image_to_string(img).lower()
-    sentence = sentence.replace("-", "")
+    linebreaks = "".join(i for i in sentence if i in correct_chars)
     sentence = " ".join("".join(i for i in sentence.replace("\n", " ") if i in correct_chars).split())
-    print("".join(sentence))
+    print(sentence)
+    return sentence, linebreaks
+
+
+def type_sentence():
+    global running
+    sentence, linebreaks = get_text_from_image()
     # Type text
     delay = 3000 / typing_speed * 2
     for char in sentence:
